4ce4692d203fc1bc88f4d2020f4b0ac9acf25da9
Aneel Grewal <aneel.grewal@hotmail.com>
2023-11-21 20:08:02 +0100

    schema & model created -> students collection created by mongodb

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ main()
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/college");
 }
+
+// creating schema for document
+const studentSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
+
+// creating model
+// model in mongoose is a class with which we construct documents.
+const Student = mongoose.model("Student", studentSchema);
